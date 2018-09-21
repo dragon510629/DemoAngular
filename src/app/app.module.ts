ccf1d0b0d2b34f25ee23566ab31649f28467e16f
Your Name <you@example.com>
2018-09-22 00:57:33 +0700

    default URL

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,11 @@ const appRoutes : Routes = [
   {
     path : 'bitcoin',
     component : BitcoinComponent
+  },
+  {
+    path : '',
+    redirectTo : '/homepage',
+    pathMatch : 'full'
   }
 ]
 
